6606d1f1ae8729bead97b7721124aee824a3925d
rpwang <ruipengwang@gmail.com>
2015-02-15 21:54:22 +0100

    new update this week
    Applied more of Foundation

--- routes/refdetail.js
+++ routes/refdetail.js
@@ -32,7 +32,7 @@ router.get('/', function(req, res, next) {
         }
         var arr_scaffoldLength = lengthDisData.Scaffold;
 
-        console.log(lengthDisData.Scaffold)
+        //console.log(lengthDisData.Scaffold)
         var arr_disData_scaffold = JSON.stringify(lengthDisData.Scaffold)
         var json_scaffold = [];
         for (var index = 0; index < arr_disData_scaffold.length;index++){
@@ -42,10 +42,26 @@ router.get('/', function(req, res, next) {
 
 	//extract json scaffold Nstats
         var json_scaffold_nstats = ref.ScaffoldStat;
-        console.log(json_scaffold_nstats.Nstats);
-        console.log(json_gapLength);
+        //console.log(json_scaffold_nstats.Nstats);
+        //console.log(json_gapLength);
+
+    //extract json contig Nstats
+        var json_contig_withn_nstats = ref.ContigStat;
+        var json_contig_withoutn_nstats = ref.ContigStat;
+
+
+
+     //extract json gap Nstats
+        var json_gap_nstats = ref.GapStat;
+
         // D3 test
         var data = [10,22,30,40,53];
+
+
+     // creating json for horizontal grouped stacked bar chart
+        var json_scaffold_stacked_bar =[];
+        foreach my 
+
         res.render('refdetail', {
             title: 'Reference Detail',
             refId: req.query.ref_id,
@@ -54,7 +70,10 @@ router.get('/', function(req, res, next) {
             json_contigLengthWithN: JSON.stringify(json_contigLengthWithN),
             json_contigLengthWithoutN: JSON.stringify(json_contigLengthWithoutN),
             json_scaffoldLengt: JSON.stringify(json_gapLength),
-            data_scaffoldNstats: json_scaffold_nstats.Nstats
+            data_scaffoldNstats: json_scaffold_nstats.Nstats,
+            data_contigWithnNstats:json_contig_withn_nstats.WithN.Nstats,
+            data_contigWithoutnNstats:json_contig_withoutn_nstats.WithoutN.Nstats,
+            data_gapNstats:json_gap_nstats.Nstats
         });
     });
 });
